4cac69843162e0063a4162a11fc330443a0699ef
Hamzakhaliq921 <abdulkhaliq920285@gmail.com>
2026-05-16 16:20:12 -0600

    fix driver errors

--- attendance_scraper.py
+++ attendance_scraper.py
@@ -24,6 +24,7 @@ import os
 # ==============================
 # ✅ YOUR CMS LOGIN DETAILS
 # ==============================
+#enter eno and password
 
 # ==============================
 # ✅ BROWSER SETUP (FIXED FOR WINDOWS)
